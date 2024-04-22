fecffab4a01785ff1e945e9be769b935ba9ef11e
abdxzi <azeezonline2013@gmail.com>
2024-04-22 19:12:18 +0530

    .

--- src/config/providerConfig.js
+++ src/config/providerConfig.js
@@ -7,7 +7,7 @@ export const projectId = 'd2018a62095ce4f319d5854857cfce2f'
 const metadata = {
   name: 'My Website',
   description: 'My Website description',
-  url: 'https://mywebsite.com', // origin must match your domain & subdomain
+  url: 'https://acc-swap.vercel.app', // origin must match your domain & subdomain
   icons: ['https://avatars.mywebsite.com/']
 }
 
